2a4dc1b3eb44a868f6d011ac91277128549d1c6b
Maksym Shyiko <maxymshyjko@gmail.com>
2024-10-06 17:02:01 +0100

    Updated env var

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,6 +1,6 @@
 
-// import { EnvironmentPlugin } from 'webpack';
+import { EnvironmentPlugin } from 'webpack';
 // const Dotenv = require('dotenv-webpack');
-// module.exports = {
-//   plugins: [new Dotenv()],
-// };
+module.exports = {
+  plugins: [new EnvironmentPlugin(['BACKEND_URL', 'MESSAGING_TOKEN', 'EMAIL_TO', 'EMAIL_FROM'])],
+};
